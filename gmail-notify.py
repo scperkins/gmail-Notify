81e12f9cad4a6382152fc4a4e89e04c480e57125
Sean Perkins <perkinsean@gmail.com>
2013-09-20 20:51:40 -0400

    added some basic pynotify

--- gmail-notify.py
+++ gmail-notify.py
@@ -1,6 +1,9 @@
+
 import feedparser
 import urllib2
 import getpass
+import sys
+import pynotify
 
 
 #get username, password
@@ -15,7 +18,7 @@ def authHandle(usr, passwd):
 	auth_handler = urllib2.HTTPBasicAuthHandler()
 	auth_handler.add_password("New mail feed", 'https://mail.google.com/', usr, passwd)
 
-def main():
+if __name__ == "__main__":
 	my_usr, my_pass = getCredentials()
 	authHandle(my_usr,my_pass)
 	#open the url with the auth handler
@@ -25,13 +28,19 @@ def main():
 	#parse dat feed
 	d = feedparser.parse(url)
 
+	if not pynotify.init("summary-body"):
+		sys.exit (1)
+
 	#print unread mail count
 	print "Number of unread emails: ", d.feed.fullcount
 
+	
 	for entry in d.entries:
-		print '/n'
-		print 'Author: ', entry.author
-		print "Subject: ", entry.title
-		print "Summary: ", entry.summary
-main()
+		n = pynotify.Notification(entry.author, entry.title, entry.summary)
+	n.show()
+	print '/n'
+	print 'Author: ', entry.author
+	print "Subject: ", entry.title
+	print "Summary: ", entry.summary
+
 
